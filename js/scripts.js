3aaffd1f2da7dfa8fee211337d223a3282be6ccc
Ali Amirhamzeh, Jennifer Batara and masha ivy <ali@>
2019-08-22 16:41:59 -0700

    not working cryptosquare

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,69 @@
 $(document).ready(function(event){
-  function squareSize(stringLength){
-    return Math.ceil(Math.sqrt(stringLength);
+  function squareSize(arrayLength){
+    return Math.ceil(Math.sqrt(arrayLength));
   }
 
   function cleanString(userInput){
-
+    var userInputLC = userInput.toLowerCase();
+    var noPunctuation = userInputLC.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var userInput = noPunctuation.split("");
+    return userInput;
   }
-  function fillMatrix(string){
+  function fillMatrix(array){
     var matrix = [];
+    var dimension = squareSize(array.length);
+    var counter = 0;
 
+    for(var i; i<dimension; i++){
+      for(var j; j<dimension; j++){
+        if(counter<string.length){
+          if(j===0){
+            matrix[i] = [];
+            matrix[i].push(array[counter]);
+            counter++;
+          }
+          else{
+            matrix[i].push(array[counter]);
+            counter++;
+          }
+        }
+        else{
+          matrix[i].push("");
+          counter++;
+        }
+      }
+    }
+    console.log(matrix);
     return matrix;
   }
 
-  function encryptedOutput(matrix){
+  function encrypt(matrix){
+    var encryptedMessageNS = [];
     var encryptedMessage = "";
+    for(var j; j<matrix.length; j++){
+      for(var i; i<matrix.length; i++){
+        encryptedMessageNS.push(matrix[i][j]);
+      }
+    }
+
+    encryptedMessageNS.forEach(function(char, i){
+      if ((i+1)%5 === 0){
+        encryptedMessage+=char = " ";
+      }
+      else{
+        encryptedMessage+=char;
+      }
+    })
 
     return encryptedMessage;
   }
 
 $("form").submit(function(event){
   event.preventDefault();
+  var userInput = $("input[name=number]").val();
+  var answer = encrypt(fillMatrix(cleanString(userInput));
+  $(".encrypted").append("<p>" + answer + "</p>");
 
-})
+});
 
-})
+});
