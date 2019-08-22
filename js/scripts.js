cec4d2cc7ef065992127b41c39e8e338f4121a27
Ali Amirhamzeh, Jennifer Batara and masha ivy <masha@>
2019-08-22 15:51:11 -0700

    initialize cryptosqure files and create bare bones js logic

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(event){
+  function squareSize(stringLength){
+    return Math.ceil(Math.sqrt(stringLength);
+  }
+
+  function cleanString(userInput){
+
+  }
+  function fillMatrix(string){
+    var matrix = [];
+
+    return matrix;
+  }
+
+  function encryptedOutput(matrix){
+    var encryptedMessage = "";
+
+    return encryptedMessage;
+  }
+
+$("form").submit(function(event){
+  event.preventDefault();
+
+})
+
+})
